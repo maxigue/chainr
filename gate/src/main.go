2fb5dfddbb97505c88485474502b6ecf3307649f
Maxime Guerreiro <mguerreiro@advanced-schema.com>
2020-04-07 19:03:15 +0200

    Add gate folder for gate micro-service
    The gate micro-service is the API gateway.

---
+++ gate/src/main.go
@@ -0,0 +1,16 @@
+// Command chainr-gate starts the chainr API Gateway.
+//
+// The gate is the entry point of chainr cloud services. It provides a coherent
+// api on top of micro-services, and adds hypermedia informations on top of
+// responses.
+package main
+
+import "log"
+
+func main() {
+	log.Println("Starting chainr-gate...")
+
+	// This makes the command wait indefinitely
+	for {
+	}
+}
