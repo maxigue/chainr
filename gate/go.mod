822783295bef7674445ec976e0bb159e34b0c5a0
Maxime Guerreiro <mguerreiro@advanced-schema.com>
2020-04-07 20:00:33 +0200

    Refactor directories to match standard project layout
    See https://github.com/golang-standards/project-layout

---
+++ gate/go.mod
@@ -0,0 +1,3 @@
+module github.com/Tyrame/chainr/gate
+
+go 1.14
--- gate/main.go
+++ gate/main.go
@@ -1,4 +1,4 @@
-// Command chainr-gate starts the chainr API Gateway.
+// Command gate starts the chainr API Gateway.
 //
 // The gate is the entry point of chainr cloud services. It provides a coherent
 // api on top of micro-services, and adds hypermedia informations on top of
@@ -8,7 +8,7 @@ package main
 import "log"
 
 func main() {
-	log.Println("Starting chainr-gate...")
+	log.Println("Starting gate...")
 
 	// This makes the command wait indefinitely
 	for {
