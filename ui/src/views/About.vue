b6b819fc1f0b89c2a8cb6add53d350115fecbe4e
Maxime Guerreiro <mguerreiro@advanced-schema.com>
2020-05-11 14:41:15 +0200

    Add the about page

--- ui/src/views/About.vue
+++ ui/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <h1>About Chainr</h1>
+    <p>
+      Chainr is a tool allowing to chain Kubernetes jobs, manage fallbacks and
+      monitor status. It is only a pet project and may not suit production
+      cases.
+    </p>
+    <p>
+      For usage information, you can see the
+      <a href="https://github.com/Tyrame/chainr" target="_blank">Github page</a
+      >.
+    </p>
+    <p>
+      If you are looking for a similar tool, battle-tested, and with a lot more
+      features, you can check out
+      <a href="https://www.spinnaker.io/" target="_blank">Spinnaker</a>.
+    </p>
   </div>
 </template>
