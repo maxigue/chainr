550e153f17a7c17890ca3aaee92b058a7bda660c
Maxime Guerreiro <mguerreiro@advanced-schema.com>
2020-05-12 17:07:59 +0200

    Add a fast mode to the chaos monkey

--- scripts/chaos.sh
+++ scripts/chaos.sh
@@ -2,11 +2,35 @@
 
 set -e
 
+usage() {
+    echo "Usage: $0 [-f]" 1>&2
+    echo "  -f: fast mode, kills pods more often"
+    exit 1
+}
+
+fast=false
+
+while getopts "f" option; do
+    case "${option}" in
+        f)
+            fast=true
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+divide=1000
+if [ "$fast" == "true" ]; then
+    divide=5000
+fi
+
 while :; do
-    sleep $(echo "scale=2; $RANDOM / 1000" | bc)
+    sleep $(echo "scale=2; $RANDOM / $divide" | bc)
     pods=($(kubectl get pods -o "jsonpath={.items[*].metadata.name}"))
     nb=${#pods[@]}
     idx=$(( RANDOM % $nb ))
     pod=${pods[$idx]}
-    kubectl delete pod $pod
+    kubectl delete pod $pod &
 done
